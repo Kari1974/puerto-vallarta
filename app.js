1f437a1c2a67f3b56b967e6d30d6f29b2ad6139a
Kari Whitney <20176403+Kari1974@users.noreply.git>
2018-06-03 17:08:43 -0700

    fetches EXIF metadata for first image.

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 window.onload = getExif;
 
-function renderImgName(content) {
-    var td = document.createElement("td");
-    td.textContent = content;
-    return td;
-}
-var checkingIt = renderImgName('functioning renderImgName()')
-console.log(checkingIt);
+// function renderImgName(content) {
+//     var td = document.createElement("td");
+//     td.textContent = content;
+//     return td;
+// }
+// var checkingIt = renderImgName('functioning renderImgName()')
+// console.log(checkingIt);
 
 function getExif() {
-    var imgFileName = document.getElementById("000-000-001");
-    EXIF.getData(img_000_000_001, function () {
-        var imgFileSize = EXIF.getTag(this, "ImageFileSize");
-        var originDateTaken = EXIF.getTag(this, "DateTimeOriginal");
-
-        var makeAndModel = document.getElementById("makeAndModel");
-        makeAndModel.innerHTML = `${make} ${model}`;
-    });
-
-    var img2 = document.getElementById("img2");
-    EXIF.getData(img2, function () {
+    var img1 = document.getElementById("000-000-001");
+    EXIF.getData(img1, function () {
         var allMetaData = EXIF.getAllTags(this);
-        var allMetaDataSpan = document.getElementById("allMetaDataSpan");
-        allMetaDataSpan.innerHTML = JSON.stringify(allMetaData, null, "\t");
     });
+
+    // var img2 = document.getElementById("img2");
+    // EXIF.getData(img2, function () {
+    //     var allMetaData = EXIF.getAllTags(this);
+    //     var allMetaDataSpan = document.getElementById("allMetaDataSpan");
+    //     allMetaDataSpan.innerHTML = JSON.stringify(allMetaData, null, "\t");
+    // });
 }
